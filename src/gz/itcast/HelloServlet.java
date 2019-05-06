d4779f8101c400b19fa6e34abcb08ccc8bc7db30
ZP <634001184@qq.com>
2019-05-06 14:32:59 +0800

    Initial commit

---
+++ src/gz/itcast/HelloServlet.java
@@ -0,0 +1,20 @@
+package gz.itcast;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        //向浏览器输出内容
+        response.getWriter().write("这是第一个动态网站的动态资源："+new Date());
+    }
+}
